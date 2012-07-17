0d1397be8ca1296dba90dd2c7d12913aeb9d0867
Sushil Damdhere <damdheresushil@gmail.com>
2012-07-17 00:49:34 -0700

    Added rails project to git repositary

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QnA::Application.config.secret_token = '420396ef6e1fdc7d4e6e0609663bef6d0ada40812239117ffac1768de2ad46c72468e055f210339f13bc9562b7803ec17ad249353b48717bf6433070a0c9587a'
